d5366545400ab3520a9923bc9727d57624bd2424
YahefuYusufu <parguzatt@gmail.com>
2024-12-02 20:04:10 +0100

    back to the stylesheet

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
 import React from "react"
 import { NavigationContainer } from "@react-navigation/native"
 import AuthNavigator from "src/app/Navigator/AuthNavigator"
-import { TamaguiProvider } from "tamagui"
-import config from "tamagui.config"
 
 export default function App() {
 	return (
-		<TamaguiProvider config={config}>
-			<NavigationContainer>
-				<AuthNavigator />
-			</NavigationContainer>
-		</TamaguiProvider>
+		<NavigationContainer>
+			<AuthNavigator />
+		</NavigationContainer>
 	)
 }
--- tailwind.comfig.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss"
-
-export default {
-	content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{ts,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-} satisfies Config
